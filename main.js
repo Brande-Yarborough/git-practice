350e9dba9ea7d2bc500a2dac7ea857d18439a377
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-26 11:24:45 -0500

    JS Outline

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 class Game {
-    constructor({player} = {}){
+  constructor({ player } = {}) {
     this.player = player;
-    }
-    
+  }
 }
+// Initialize with blank grid.
+// Listen for click on each grid element.
+// First click gets an X.
+// Second click gets an O.
+
+// Look at the grid and check if there is alignment for any shapes.
